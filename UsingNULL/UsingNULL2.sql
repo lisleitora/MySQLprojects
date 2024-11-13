f7edb0d80e74a8478f5d8450a761b76aa44f4940
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-11-13 14:43:32 +0000

    Using NULL

---
+++ UsingNULL/UsingNULL2.sql
@@ -0,0 +1,5 @@
+-- Note the INNER JOIN misses the teachers with no department and the departments with no teacher.
+
+SELECT teacher.name, dept.name
+ FROM teacher INNER JOIN dept
+           ON (teacher.dept=dept.id)
---
+++ UsingNULL/UsingNULL5.sql
@@ -0,0 +1,4 @@
+-- Use COALESCE to print the mobile number. Use the number '07986 444 2266' if there is no number given. Show teacher name and mobile number or '07986 444 2266'
+
+SELECT name, COALESCE(mobile,'07986 444 2266')
+ FROM teacher
