f0689489a829514c5ab9fda3b24f2416dceb04bf
lisLeitora <62620405+lisleitora@users.noreply.github.com>
2024-11-25 15:05:07 +0000

    final

--- SQLMystery/Report.sql
+++ SQLMystery/Report.sql
@@ -26,3 +26,38 @@ JOIN drivers_license ON (license_id = drivers_license.id)
 
 -- 14887	Morty Schapiro	118009	4919	Northwestern Dr	111564949
 -- I heard a gunshot and then saw a man run out. He had a "Get Fit Now Gym" bag. The membership number on the bag started with "48Z". Only gold members have those bags. The man got into a car with a plate that included "H42W".
+ 
+ SELECT * FROM person 
+WHERE id = '67318'
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+-- !!!! ASSASSIN !!!!! 67318	Jeremy Bowers	423327	530	Washington Pl, Apt 3A	871539279
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+ SELECT * FROM interview 
+WHERE person_id = '67318'
+--I was hired by a woman with a lot of money.
+-- I don't know her name but I know she's around 5'5" (65") or 5'7" (67").
+-- She has red hair and she drives a Tesla Model S. 
+--I know that she attended the SQL Symphony Concert 3 times in December 2017.
+
+
+
+SELECT * FROM drivers_license
+WHERE gender = 'female' 
+AND hair_color = 'red'
+AND car_make = 'Tesla'
+AND car_model = 'Model S'
+
+
+SELECT * FROM facebook_event_checkin
+JOIN person ON (person_id = person.id)
+JOIN drivers_license ON (license_id = drivers_license.id)
+WHERE event_id = '1143' AND date = '20171206' AND gender = 'female'
+
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+-- !!!! HIRER !!!!! 99716	1143	SQL Symphony Concert	20171206	99716	Miranda Priestly	202298	1883	Golden Ave	987756388	202298	68	66	green	red	female	500123	Tesla	Model S
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
